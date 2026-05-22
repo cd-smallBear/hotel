012a777676dccc871872918150e832750b5445ef
DESKTOP-QDMN4DH\Admin <287543429@qq.com>
2026-05-22 19:14:30 +0800

    1

--- docs/js/component/tag.js
+++
@@ -1,9 +0,0 @@
-define([],function(){
-   return function(JQdom,callback){
-     JQdom.on("click",".tag",function(){
-          var _this = $(this);
-              _this.toggleClass("active");
-              $.isFunction(callback) && callback(_this);
-     })
-   }
-})
--- docs/js/pages/login.js
+++
@@ -1,8 +0,0 @@
-$("#form-submit").click(function(){
-  this.textContent= this.textContent + "中...";
-  var rand = Math.random() * 1000 + 500;
-  setTimeout(function(){
-    location.href= HotelConfig.root + "/room/index.html";
-  },rand);
-  return false;
-});
